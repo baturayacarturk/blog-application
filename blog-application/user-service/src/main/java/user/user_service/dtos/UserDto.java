885d1621a7069c4a279a36a21444072cd81fb52b
Baturay Acarturk <baturayacarturk@outlook.com>
2024-09-09 11:56:36 +0300

    create repositories

---
+++ blog-application/user-service/src/main/java/user/user_service/dtos/UserDto.java
@@ -0,0 +1,14 @@
+package user.user_service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+
+}
