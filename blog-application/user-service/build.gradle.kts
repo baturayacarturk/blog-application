16a6a70bdee68ea0fbf7e2be9542ff78f1c32347
Baturay Acarturk <baturayacarturk@outlook.com>
2024-09-09 11:50:10 +0300

    user-service project created

---
+++ blog-application/user-service/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.18"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("org.flywaydb.flyway") version "9.8.2"
+}
+
+group = "user"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.postgresql:postgresql")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
